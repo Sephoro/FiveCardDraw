7b1e3ffd0cd45666e76d3b0f8e10ff711ff971ca
sephoro <eliassepuru@gmail.com>
2023-11-02 17:30:31 +0200

    Clear logs after compiling also check if maven clean install was a success

--- run.sh
+++ run.sh
@@ -8,6 +8,24 @@ JAR_FILE="poker-0.0.1-SNAPSHOT.jar"
 
 ./mvnw clean install
 
-# Run the jar file
+# Check if the Maven build was successful
+if [ $? -eq 0 ]; then
 
-java -jar target/$JAR_FILE
+    # Clear The Screen from all the logs
+    # Detect the operating system
+    if [ "$(uname)" == "Darwin" ]; then
+        # Mac OS X
+        clear
+    elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+        # Linux
+        clear
+    elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ] || [ "$(expr substr $(uname -s) 1 10)" == "MSYS_NT-10" ]; then
+        # Windows with MSYS2 (Git Bash, Cygwin, etc.)
+        cls
+    fi
+
+    # Run the Spring Boot application
+    java -jar target/$JAR_FILE
+else
+    echo "Maven build failed. Please fix the issues before running the application."
+fi
